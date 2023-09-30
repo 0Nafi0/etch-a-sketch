8fb96f77d14cc0ec88e00f558941b1444cf6b8f5
Nafi <128926697+0Nafi0@users.noreply.github.com>
2023-09-30 20:30:18 +0600

    Addition of ui and the sketchboard
    Only designing of the button and dynamic changing of the squares
    when the user clicks on it

---
+++ js/javascript.js
@@ -0,0 +1,30 @@
+let grid = 16;
+const parentContainer = document.querySelector(".parent");
+const body = document.querySelector("body");
+function createNewDiv() {
+    const newDiv = document.createElement("div");
+    const side = (800 / grid).toString() + "px";
+    newDiv.style.width = side;
+    newDiv.style.height = side;
+    parentContainer.appendChild(newDiv);
+}
+
+function divCreation(grid) {
+    for (let i = 0; i < (grid ** 2); i++)
+    {
+        createNewDiv();
+    }
+}
+
+divCreation(grid);
+
+const gridButton = document.querySelector("button");
+
+gridButton.addEventListener("click", () => {
+    grid = parseInt(prompt("Enter the number of grid you want on each row: "));
+    const childDivs = document.querySelectorAll(".parent > div");
+    childDivs.forEach((childDiv) => {
+        parentContainer.removeChild(childDiv);
+    })
+    divCreation(grid);
+})
